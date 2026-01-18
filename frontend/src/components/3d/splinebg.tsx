e378de851bf3109f00df4a8884d47d64e046543b
priyanshunitr <sahupriyanshu2006@gmail.com>
2026-01-18 05:40:05 +0530

    3d effect with spline complete

--- frontend/src/components/3d/splinebg.tsx
+++ frontend/src/components/3d/splinebg.tsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 
 export default function SplineBg() {
   return (
-    <div>
+    <div className="w-full h-full">
       <Spline scene="https://prod.spline.design/eptFcGbRN8-SmKyB/scene.splinecode" />
     </div>
   );
